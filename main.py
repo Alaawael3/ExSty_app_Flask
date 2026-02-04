7db88cb831aef5a1137cb25e3c31749f1e02cbc3
alaa wael <111670724+Alaawael3@users.noreply.github.com>
2026-02-04 22:17:11 +0200

    Enable debug mode for app execution

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ app = create_app()
 # with current_app.app_context():
 #     db.create_all()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+if __name__ == '__main__':
+    app.run(debug=True)
