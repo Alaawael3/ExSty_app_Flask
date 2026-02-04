7ccdaea9a15e361d3bde2458cdeac6667e6a307f
alaa wael <111670724+Alaawael3@users.noreply.github.com>
2026-02-05 01:30:33 +0200

    Second commit - Flask app

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from Exsty_app import create_app
-from flask import current_app
-from Exsty_app import db
+from Exsty_app import create_app, db
 
 app = create_app()
 
-with current_app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
