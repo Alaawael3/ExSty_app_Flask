7dc80294cf8425f38857458ac356ddb8742eea45
alaa wael <111670724+Alaawael3@users.noreply.github.com>
2026-02-04 22:14:03 +0200

    Update app.run to use host and port from environment

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ app = create_app()
 # with current_app.app_context():
 #     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
