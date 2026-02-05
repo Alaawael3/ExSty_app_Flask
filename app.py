c19f44187d6f1d841fadd9f5d3c8353972b50f79
alaa wael <111670724+Alaawael3@users.noreply.github.com>
2026-02-05 02:36:14 +0200

    Implement CLI command to create database tables
    Added a CLI command to create database tables.

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import click
+from flask.cli import with_appcontext
 from Exsty_app import create_app, db
 
 app = create_app()
 
-# with app.app_context():
-#     db.create_all()
+@click.command(name="create_tables")
+@with_appcontext
+def create_tables():
+    db.create_all()
+    click.echo("Tables created successfully!")
+
+app.cli.add_command(create_tables)
 
 if __name__ == '__main__':
     app.run(debug=True)
